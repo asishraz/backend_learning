33728a0909152f442cf57b266f140a6939604b37
asishraz <iamasish3001@gmail.com>
2021-08-03 22:14:28 +0530

    file added

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello");
+print("create account now")
+
+user_name = input("Enter your name: ")
+user_pwd = input("Enter your password: ")
+
+print("your account has been created successfully!")
+print("Login Now")
+
+
+user_name2 = input("enter your name: ")
+user_pwd2 = input("enter your password: ")
+
+if user_name == user_name2 and user_pwd == user_pwd2:
+    print("login successful")
+else:
+    print("Invalid credentials")
